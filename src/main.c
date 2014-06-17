d738b7fc36cb21313496541d83688c4ca042d378
Tom Verloop <Tom_Verloop@live.nl>
2014-06-17 14:53:24 +0200

    Serial Test Data.

--- src/main.c
+++ src/main.c
@@ -7,28 +7,80 @@
 ///														 					 ///
 ///--------------------------------------------------------------------------///
 
-
 #include "LPC11xx.h"
 
-int main(void) {
-	unsigned char x = 0x01;
-    //SET UP UART (sec. 13.2 in datasheet "BASIC CONFIGURATION")
-    LPC_IOCON->PIO1_7         |= 0x01;       //configure UART TXD pin (sec. 7.4.41)
-    LPC_SYSCON->SYSAHBCLKCTRL |= (1<<12);    //enable clock to UART (sec. 3.5.14)
-    LPC_SYSCON->UARTCLKDIV    |= x;       //0x9B will give approx. 19.2K baud signal (sec. 3.5.16)
-    LPC_UART->FCR             |= 0x01;       //enable UART FIFOs (necessary for operation) (sec. 13.5.6)
-    LPC_UART->LCR             |= 0x03;       //set for 8 bit data width (sec. 13.5.7)
-    LPC_UART->TER             |= 0x80;       //transmit enable (sec. 13.5.16)
+const int Hz = 1;
+const int kHz = 1000;
+const int MHz = 1000 * 1000;
 
-    //unsigned int i = 0;
+const unsigned int LSR_RDR = 0x01;
+const unsigned int LSR_OE = 0x02;
+const unsigned int LSR_PE = 0x04;
+const unsigned int LSR_FE = 0x08;
+const unsigned int LSR_BI = 0x10;
+const unsigned int LSR_THRE = 0x20;
+const unsigned int LSR_TEMT = 0x40;
+const unsigned int LSR_RXFE = 0x80;
+const int clock_frequency = 12 * 1000 * 1000;
+const int baudrate = 38400;
 
-    while(1){                    //infinite loop
+void baudrate_set(unsigned int baud)
+{
+	int Fdiv;
+	LPC_SYSCON->UARTCLKDIV = 0x1;     // divided by 1
+	Fdiv = clock_frequency / ( LPC_SYSCON->SYSAHBCLKDIV * 16 * baud);
+	LPC_UART->DLM = Fdiv / 256;
+	LPC_UART->DLL = Fdiv % 256;
+}
 
-         LPC_UART->THR |= x;              //transmit data (sec. 13.5.2)
-         x++;
+int main(void)
+{
 
-         LPC_SYSCON->UARTCLKDIV    &= ~0xFF;
-         LPC_SYSCON->UARTCLKDIV    |= x;       //0x9B will give approx. 19.2K baud signal (sec. 3.5.16)
-    }
-    return 0 ;
+	//SET UP UART (section 13.2 in datasheet "BASIC CONFIGURATION")
+	// enable IO config
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 16);     //enable IOCON
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 12);     //enable UART
+
+	// UART I/O config
+	LPC_IOCON->PIO1_6 &= ~0x07;
+	LPC_IOCON->PIO1_6 |= 0x01;     // UART RXD
+	LPC_IOCON->PIO1_7 &= ~0x07;
+	LPC_IOCON->PIO1_7 |= 0x01;     // UART TXD
+
+	// Enable UART clock
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 12);
+
+	LPC_UART->LCR = 0x83;             // 8 bits, no Parity, 1 Stop bit
+	baudrate_set(38400);
+	LPC_UART->LCR = 0x03;   // DLAB = 0
+	LPC_UART->FCR = 0x07;   // Enable and reset TX and RX FIFO.
+
+	// Read to clear the line status.
+	(void) LPC_UART->LSR;
+
+	// Ensure a clean start, no data in either TX or RX FIFO.
+	while ((LPC_UART->LSR & (LSR_THRE | LSR_TEMT)) != (LSR_THRE | LSR_TEMT))
+		;
+	while ( LPC_UART->LSR & LSR_RDR)
+	{
+		(void) LPC_UART->RBR; // Dump data from RX FIFO
+	}
+
+	unsigned int i = 0;
+	unsigned int data = 0x55;
+
+	while (1)
+	{                 //infinite loop
+
+		LPC_UART->THR |= data & 0xFF;     //transmit data (sec 13.5.2)
+		for (i = 0; i < 0x3FFFF; ++i)
+			;             //arbitrary delay
+		while (1)
+		{               //wait for transmitted byte to loop back and be received
+			if (LPC_UART->LSR & 0x01) //if Receiver Data Ready bit set (sec 13.5.9)
+				break;
+		}
+		data = LPC_UART->RBR;               //store received data (sec 13.5.1)
+	}
+	return 0;
 }
